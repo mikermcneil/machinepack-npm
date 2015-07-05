13404ce02f6423c7be0bf5fe48482141365da210
Mike McNeil <mike@balderdash.co>
2015-07-05 14:18:20 -0500

    Clean up some metadata, and add support a new 'dir' input.

--- machines/install-package.js
+++ machines/install-package.js
@@ -1,9 +1,9 @@
 module.exports = {
-  
-  
+
+
   friendlyName: 'Install package',
-  
-  
+
+
   description: 'Install a package from the NPM registry to the node_modules folder of a local project.',
 
 
@@ -14,109 +14,100 @@ module.exports = {
 
     name: {
       friendlyName: 'Package name',
-      description: 'The name of the NPM package to install. Can also be a custom url to a package.',
+      description: 'The name of the NPM package to install.',
+      extendedDescription: 'Can also be a custom url to a package.',
       example: 'express',
       required: true,
     },
 
     version: {
-      friendlyName: 'Package version',
-      description: 'The semantic version of the NPM package to install',
-      example: '~1.0.0'
+      friendlyName: 'Version range',
+      description: 'A version string (or semver range) of the NPM package to install.',
+      example: '~1.0.0',
+      defaultsTo: '*'
+    },
+
+    dir: {
+      friendlyName: 'Directory',
+      description: 'The local path where the package should be installed as a dependency.',
+      extendedDescription: 'This is to the local package itself-- NOT its node_modules folder! Also note that, if specified as a relative path, this will be resolved relative to the current working directory.  If unspecified, the current working directory will be used.',
+      example: '/Users/mikermcneil/dogfood-promo-site'
     },
 
     save: {
-      friendlyName: '--save flag',
-      description: 'Installs the npm package with the --save flag enabled',
-      example: true
+      friendlyName: 'Save dependency?',
+      description: 'If set, the installed package will be saved to the package.json file as a dependency.',
+      extendedDescription: 'This runs `npm install` with the --save flag enabled.',
+      example: true,
+      defaultsTo: false
     },
 
     saveDev: {
-      friendlyName: '--save-dev flag',
-      description: 'Installs the npm package with the --save-dev flag enabled',
-      example: true
+      friendlyName: 'Save dev dependency?',
+      description: 'If set, the installed package will be saved to the package.json file as a development-only dependency.',
+      extendedDescription: 'This runs `npm install` with the --save-dev flag enabled.',
+      example: true,
+      defaultsTo: false
     },
 
     prefix: {
-      friendlyName: '--prefix flag',
-      description: 'Changes the root path to where your npm package gets installed (it will still be installed within node_modules of that folder).',
+      friendlyName: 'Prefix',
+      description: 'An optional path prefix which affects the root path in which this NPM package gets installed.',
+      extendedDescription: 'Note that the package will still be installed within node_modules of the destination directory. This just controls the `--prefix` command-line option passed to `npm install`.',
       example: './path-to-project'
     },
 
     loglevel: {
-      friendlyName: '--loglevel flag',
-      description: 'Changes the loglevel of npm. silent, warn, verbose and silly are options.',
-      example: 'warn'
+      friendlyName: 'Log level',
+      description: 'If set, NPM will write logs to the console at the specified log level.',
+      extendedDescription: 'This controls the --loglevel flag passed to `npm install`. Available options are "silent", "warn", "verbose" and "silly".',
+      example: 'warn',
+      defaultsTo: 'silent'
     }
+
   },
 
 
   exits: {
 
     success: {
-      description: 'Package successfully installed',
-      extendedDescription: 'The package will be installed locally within',
-      example: 'lodash',
-      variableName: 'installedPackageName'
-    },
-
-    error: {
-      description: 'Unexpected error occurred.',
-      variableName: 'err'
+      description: 'Package successfully installed.'
     },
 
     invalidSemVer: {
-      description: 'Passed an invalid semantic version number. See https://docs.npmjs.com/misc/semver for details.'
+      description: 'Provided semver range is invalid. See https://docs.npmjs.com/misc/semver for more information.'
     }
+
   },
 
   fn: function(inputs, exits) {
-
-    /**
-     * Dependencies
-     */
-
-    var npm = require('enpeem');
+    var path = require('path');
+    var enpeem = require('enpeem');
     var semver = require('semver');
 
+    // Resolve provided path to ensure it is absolute
+    // (and default to cwd if it was left unspecified)
+    inputs.dir = inputs.dir ? path.resolve(inputs.dir) : process.cwd();
 
-    var pkg = inputs.name;
-
-    //check if version is passed
-    if ('version' in inputs && inputs.version.length > 0) {
-      //check if valid semantic version number or range
-      if(semver.validRange(inputs.version)) {
-        pkg += '@' + inputs.version
-      } else {
-        return exits.invalidSemVer();
-      }
+    // Validate provided semver range.
+    if (!semver.validRange(inputs.version)) {
+      return exits.invalidSemVer();
     }
 
-    var npmSave = false;
-    if ('save' in inputs && inputs.save) {
-      npmSave = true;
-    }
-
-    var npmSaveDev = false;
-    if ('saveDev' in inputs && inputs.saveDev) {
-      npmSaveDev = true;
-    }
-
-    //console.log(process.cwd());
-
-    //install the npm package
-    npm.install({
+    // Install the npm package
+    enpeem.install({
       dependencies: [
-        pkg
+        inputs.name + '@' + inputs.version
       ],
-      save: npmSave,
-      saveDev: npmSaveDev,
-      loglevel: inputs.loglevel || 'silent',
-      prefix: inputs.prefix || undefined,
+      dir: inputs.dir,
+      save: inputs.save,
+      saveDev: inputs.saveDev,
+      loglevel: inputs.loglevel,
+      prefix: inputs.prefix,
       'cache-min': 999999999
     }, function (err) {
       if (err) { return exits.error(err); }
-      return exits.success(pkg);
+      return exits.success();
     });
 
   }
