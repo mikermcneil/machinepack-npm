4c5431d3067354838defad78b921e2acd9d60839
Mike McNeil <mike@balderdash.co>
2016-07-26 13:14:08 -0500

    Cleanup and updates.

--- machines/download-package.js
+++ machines/download-package.js
@@ -27,10 +27,10 @@ module.exports = {
     },
 
     registry: {
-      "friendlyName": "Registry URL",
-      "description": "The URL of the NPM registry to use (defaults to public npm registry)",
-      "defaultsTo": "https://registry.npmjs.org",
-      "example": "https://your-private-registry.npmjs.org"
+      friendlyName: 'Registry URL',
+      description: 'The URL of the NPM registry to use (defaults to the public npm registry)',
+      example: 'https://your-private-registry.npmjs.org',
+      defaultsTo: 'https://registry.npmjs.org',
     }
 
   },
@@ -39,15 +39,11 @@ module.exports = {
   exits: {
 
     success: {
-      description: 'Package successfully downloaded',
+      description: 'Package successfully downloaded.',
       extendedDescription: 'The untar\'ed files will be stored in the operating system\'s temporary directory under a folder given by the `name` and `version` inputs.  e.g. if `name` is "foobar" and `version` is "0.2.1", then the untar\'ed files might be in "/tmp/foobar-0.2.1/package/*".  This exit returns the absolute path to the folder of untar\'ed goodies.',
+      outputFriendlyName: 'Path',
+      outputDescription: 'The path to the downloaded NPM package on disk.',
       example: '/var/folders/_s/347n05_x2rgb_0w6s6y0ytr00000gn/T/machinepack-phantomjscloud-0.1.2',
-      variableName: 'pathToDownloadedPkg'
-    },
-
-    error: {
-      description: 'Unexpected error occurred.',
-      variableName: 'err'
     },
 
     untarFailed: {
@@ -69,11 +65,6 @@ module.exports = {
 
 
   fn: function(inputs, exits) {
-
-    /**
-     * Dependencies
-     */
-
     var request = require('request');
     var os = require('os');
     var path = require('path');
@@ -83,7 +74,7 @@ module.exports = {
     var semver = require('semver');
 
     if (!semver.valid(inputs.version)) {
-      return exit.invalidSemVer();
+      return exits.invalidSemVer();
     }
 
     var remoteFilename = inputs.name + '-' + inputs.version + '.tgz';
@@ -106,27 +97,27 @@ module.exports = {
       },
     }).execSync();
 
-    (function (cb){
+    (function (done){
       var gunner = zlib.createGunzip()
       .once('error', function (err){
-        return cb(err||new Error('gzip extraction error'));
+        return done(err||new Error('gzip extraction error'));
       });
 
       var extractor = tar.Extract({ path: tmp })
       .once('error', function (err){
-        return cb(err||new Error('tar extraction error'));
+        return done(err||new Error('tar extraction error'));
       })
       .once('end', function() {
-        return cb(null, tmp + '/package');
+        return done(null, tmp + '/package');
       });
 
       var httpClientRequest = request.get(uri);
       httpClientRequest.once('error', function (err){
-        return cb(err||new Error('HTTP request stream error'));
+        return done(err||new Error('HTTP request stream error'));
       });
       httpClientRequest.pipe(gunner).pipe(extractor);
     })(function afterwards(err, resultPath){
-      if (err) return exits.error(err);
+      if (err) { return exits.error(err); }
       return exits.success(resultPath);
     });
   }
