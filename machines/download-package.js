8c96fe8ab91c95a2b2ee2a3e85d7f41dcdb51c7f
Mike McNeil <mike@balderdash.co>
2016-07-26 13:33:23 -0500

    Allow destination path to be passed in to downloadPackage().

--- machines/download-package.js
+++ machines/download-package.js
@@ -31,7 +31,16 @@ module.exports = {
       description: 'The URL of the NPM registry to use (defaults to the public npm registry)',
       example: 'https://your-private-registry.npmjs.org',
       defaultsTo: 'https://registry.npmjs.org',
-    }
+    },
+
+    destination: {
+      description: 'The absolute path where the package should be extracted.',
+      extendedDescription:
+      'If a relative path is specified, it will be resolved relative to cwd.\n'+
+      'If left unspecified, the package will be extracted into a new subfolder '+
+      'within the operating system\'s `tmp/` directory.',
+      example: '/Users/mikermcneil/dogfood-promo-site'
+    },
 
   },
 
@@ -77,10 +86,6 @@ module.exports = {
       return exits.invalidSemVer();
     }
 
-    var remoteFilename = inputs.name + '-' + inputs.version + '.tgz';
-    var folderName = inputs.name + '-' + inputs.version;
-    var tmp = path.resolve(os.tmpDir(), folderName);
-
 
     // Build a sanitized version of the provided registry URL (i.e. with "http://")
     // (default to public NPM registry)
@@ -89,6 +94,7 @@ module.exports = {
     }).execSync();
 
     // Build the URL where the remote package tarball lives.
+    var remoteFilename = inputs.name + '-' + inputs.version + '.tgz';
     var uri = Urls.buildUrlFromTemplate({
       urlTemplate: registryUrl+'/:name/-/:remoteFilename',
       data: {
@@ -97,22 +103,46 @@ module.exports = {
       },
     }).execSync();
 
-    (function (done){
+    (function _requestPkgFromNPMAndExtractResAsStream(done){
+      // Set up a spinlock, just to be safe.
+      var alreadyFinished;
+
+      // Figure out the appropriate path to a directory where the package will
+      // be extracted after it is downloaded.  If no explicit `destination` path
+      // was specified, then use the path to a new subfolder within the operating
+      // system's `/tmp` directory.
+      var destinationPath = (function (){
+        if (typeof inputs.destination === 'undefined') {
+          return path.resolve(inputs.destination);
+        }
+        else {
+          return path.resolve(os.tmpDir(), inputs.name + '-' + inputs.version);
+        }
+      })();
+
       var gunner = zlib.createGunzip()
       .once('error', function (err){
+        if (alreadyFinished) { return; }
+        alreadyFinished = true;
         return done(err||new Error('gzip extraction error'));
       });
 
-      var extractor = tar.Extract({ path: tmp })
+      var extractor = tar.Extract({ path: destinationPath })
       .once('error', function (err){
+        if (alreadyFinished) { return; }
+        alreadyFinished = true;
         return done(err||new Error('tar extraction error'));
       })
       .once('end', function() {
-        return done(null, tmp + '/package');
+        if (alreadyFinished) { return; }
+        alreadyFinished = true;
+        return done(null, path.join(destinationPath, 'package'));
       });
 
       var httpClientRequest = request.get(uri);
       httpClientRequest.once('error', function (err){
+        if (alreadyFinished) { return; }
+        alreadyFinished = true;
         return done(err||new Error('HTTP request stream error'));
       });
       httpClientRequest.pipe(gunner).pipe(extractor);
