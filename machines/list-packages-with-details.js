7f181b3aa0f6327a97076282343e165d6b7b20b9
Mike McNeil <mike@balderdash.co>
2016-09-19 10:01:52 -0500

    Fix listPackagesWithDetails() to make tests pass.

--- machines/list-packages-with-details.js
+++ machines/list-packages-with-details.js
@@ -1,85 +1,119 @@
 module.exports = {
+
+
   friendlyName: 'List packages (detailed)',
+
+
   description: 'List matching packages and include metadata from their package.json files.',
+
+
   extendedDescription: 'Also includes the raw package.json string as `rawJson`, in case you need to parse additional non-standard metadata.',
+
+
   cacheable: true,
+
+
   inputs: {
+
     query: {
       example: 'sails',
-      description: 'A string that will be used when querying npm.',
+      description: 'The search query to send to NPM.',
       extendedDescription: 'This string will be used to search within package keywords.',
       required: true
     }
+
   },
-  defaultExit: 'success',
+
+
   exits: {
-    error: {
-      description: 'Unexpected error occurred'
-    },
+
     success: {
-      example: [ {
-        "name": "browserify",
-        "description": "asg",
-        "version": "0.1.1",
-        "keywords": ["machine"],
-        "latestVersionPublishedAt": "2015-01-19T22:26:54.588Z",
-        "npmUrl": "http://npmjs.org/package/machinepack-foo",
-        "sourceUrl": "https://github.com/baz/machinepack-foo",
-        "author": "Substack <substack@substack.com>",
-        "main": "lib/index.js",
-        "dependencies": [{
-          "name": "lodash",
-          "semverRange": "^2.4.1"
-        }],
-        "license": "MIT",
-        "contributors": [{
-          "name": "Substack",
-          "email": "substack@substack.com"
-        }],
-        "rawJson": "{...package.json data as a JSON string...}"
-      } ]
-    }
+      outputDescription: 'An array of package detail dictionaries for each of the matching NPM packages.',
+      example: [
+        {
+          name: 'browserify',
+          description: 'asg',
+          version: '0.1.1',
+          latestVersionPublishedAt: '2015-01-19T22:26:54.588Z',
+          npmUrl: 'http://npmjs.org/package/machinepack-foo',
+          sourceUrl: 'https://github.com/baz/machinepack-foo',
+          author: 'Substack <substack@substack.com>',
+          main: 'lib/index.js',
+          license: 'MIT',
+          rawJson: '{...package.json data as a JSON string...}',
+          keywords: [
+            'machine'
+          ],
+          contributors: [
+            { name: 'Substack', email: 'substack@substack.com' }
+          ],
+          dependencies: [
+            { name: 'lodash', semverRange: '^2.4.1' }
+          ]
+        }
+      ]
+    }//</success>
+
   },
-  fn: function (inputs, cb){
+
+
+  fn: function (inputs, exits){
 
     var async = require('async');
     var Machine = require('machine');
+    var listPackages = Machine.build(require('./list-packages'));
+    var fetchInfo = Machine.build(require('./fetch-info'));
+    var parsePackageJson = Machine.build(require('./parse-package-json'));
 
-    Machine.build(require('./list-packages'))({
+
+    listPackages({
       query: inputs.query
     }).exec(function (err, npmPackageNames) {
-      if (err) return cb(err);
+      if (err) { return exits.error(err); }
 
       // Now expand each module with full results directly
       // from the npm registry.
       var npmModules = [];
 
-      async.each(npmPackageNames, function (packageName, next) {
-        Machine.build(require('./fetch-info'))({
-          packageName: packageName
-        }).exec(function(err, packageJsonString) {
-          if (err) return next(err);
+      async.each(
+        npmPackageNames,
 
-          var metadata;
-          try {
-            metadata = Machine.build(require('./parse-package-json'))({
-              json: packageJsonString
-            }).execSync();
-            // Attach raw json string
-            metadata.rawJson = packageJsonString;
-          }
-          catch (e){
-            return next(e);
-          }
-          npmModules.push(metadata);
-          return next();
-        });
-      }, function (err) {
-        if (err) return cb(err);
-        return cb(null, npmModules);
-      });
-    });
+        function iteratee(packageName, next) {
 
+          fetchInfo({
+            packageName: packageName
+          }).exec(function (err, pkgInfo) {
+            if (err) { return next(err); }
+
+            var metadata;
+            try {
+
+              var rawJsonStr = JSON.stringify(pkgInfo);
+
+              metadata = parsePackageJson({
+                json: rawJsonStr
+              }).execSync();
+
+              // Attach raw json string
+              metadata.rawJson = rawJsonStr;
+
+            } catch (e){ return next(e); }
+
+            npmModules.push(metadata);
+
+            return next();
+
+          });
+        },//</iteratee>
+
+        // ~∞%°
+        function afterwards(err) {
+          if (err) { return exits.error(err); }
+          return exits.success(npmModules);
+        }
+      );//</async.each>
+    });//</listPackages()>
   }
+
 
 };
